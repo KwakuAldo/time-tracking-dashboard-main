1631d47882d90342d6c2b6632f9662fcbff888ab
KwakuAldo <x.pac20032001@gmail.com>
2023-01-19 13:54:16 +0000

    updating html with data from json

--- script/script.js
+++ script/script.js
@@ -1,17 +1,9 @@
-function loadJSON() {
-    fetch('data.json')
-        .then(response => response.json())
-        .then(data => {
-            //update the element with title
-            document.getElementById("title").innerHTML = data[0].title;
-
-            //update the element with daily current
-            document.getElementById("dailyCurrent").innerText = data[0].timeframes.daily.current;
-
-            //update the element with daily previous
-            document.getElementById("dailyPrevious").innerText = data[0].timeframes.daily.previous;
-        })
-        .catch(error => console.error(error))
-}
+fetch("data.json")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data[5].timeframes.daily);
+        document.querySelector(".current").innerHTML = data[0].timeframes.daily.current + "hrs";
+        document.querySelector(".previous").innerHTML = "Yesterday - " + data[1].timeframes.daily.previous + "hrs";
+    })
 
 console.log('loaded script')
