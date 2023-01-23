df7fc872620c8083b6bcda6b51b89def8cad62c9
KwakuAldo <x.pac20032001@gmail.com>
2023-01-23 15:23:26 +0000

    Added mobile design

--- script/script.js
+++ script/script.js
@@ -1,9 +1,9 @@
-fetch("data.json")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data[5].timeframes.daily);
-        document.querySelector(".current").innerHTML = data[0].timeframes.daily.current + "hrs";
-        document.querySelector(".previous").innerHTML = "Yesterday - " + data[1].timeframes.daily.previous + "hrs";
-    })
+// fetch("data.json")
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data[5].timeframes.daily);
+//         document.querySelector(".current").innerHTML = data[0].timeframes.daily.current + "hrs";
+//         document.querySelector(".previous").innerHTML = "Yesterday - " + data[1].timeframes.daily.previous + "hrs";
+//     })
 
 console.log('loaded script')
